a37cbc1607c224f3a9c753b11a0750216f44d596
RajMadhok <43905953+RajMadhok@users.noreply.github.com>
2018-11-02 20:59:12 -0700

    Add files via upload

---
+++ average.py
@@ -0,0 +1,7 @@
+n1=int(input("enter 1st number: "))
+n2=int(input("enter 2nd number: "))
+n3=int(input("enter 3rd number: "))
+n4=int(input("enter 4th number: "))
+n5=int(input("enter 5th number: "))
+average=(n1+n2+n3+n4+n5)/5
+print("the average of 5 numbers are :",average)
